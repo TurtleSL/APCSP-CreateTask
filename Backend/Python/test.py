3abc2a6610b95c7e712a44eeaa068ab1764110e7
PyricRL <ljcosa@icloud.com>
2025-04-01 12:51:05 -0500

    python data works

--- Backend/Python/test.py
+++ Backend/Python/test.py
@@ -9,22 +9,20 @@ array = [0, 2, 5, 3, 4, 1]
 
 data = []
 
+def bubbleSort(array):
+    global sortTime
+    startTime = time.perf_counter()
+    for i in range(len(array)):
+            for j in range(0, len(array) - i - 1):
+                if array[j] > array[j + 1]:
+                    array[j], array[j + 1] = array[j + 1], array[j]
+                    sortTime = time.perf_counter() - startTime
+                    Time = f"t{sortTime:.10f}"
+                    with open(FILE, mode='a', newline='') as file:
+                        writer = csv.writer(file)
+                        writer.writerow(array + [Time])
+
 with open(FILE, mode='w', newline='') as file:
-    writer = csv.writer(file)
+    pass
 
-    array_str = str(array)
-    
-    writer.writerow(array_str, 0.02)
-
-# def bubbleSort(array):
-#     global sortTime
-#     startTime = time.perf_counter()
-#     for i in range(len(array)):
-#             for j in range(0, len(array) - i - 1):
-#                 if array[j] > array[j + 1]:
-#                     array[j], array[j + 1] = array[j + 1], array[j]
-#                     sortTime = time.perf_counter() - startTime
-#                     data.update(array : sortTime)
-#                     print(data)
-
-#bubbleSort(array)
+bubbleSort(array)
