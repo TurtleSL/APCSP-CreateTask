ee3d7acf44a7519d796093409aacc1220de30f4a
PyricRL <ljcosa@icloud.com>
2025-04-01 11:31:47 -0500

    adjusted some stuff

--- Backend/Python/test.py
+++ Backend/Python/test.py
@@ -1,23 +1,30 @@
-import json
+import csv
 import time
 
-FILE = "data.json"
+FILE = "pythonData.csv"
 
 sortTime = 0
 
 array = [0, 2, 5, 3, 4, 1]
 
-data = {}
+data = []
 
-def bubbleSort(array):
-    global sortTime
-    startTime = time.perf_counter()
-    for i in range(len(array)):
-            for j in range(0, len(array) - i - 1):
-                if array[j] > array[j + 1]:
-                    array[j], array[j + 1] = array[j + 1], array[j]
-                    sortTime = time.perf_counter() - startTime
-                    data.update(array : sortTime)
-                    print(data)
+with open(FILE, mode='w', newline='') as file:
+    writer = csv.writer(file)
 
-bubbleSort(array)
+    array_str = str(array)
+    
+    writer.writerow(array_str, 0.02)
+
+# def bubbleSort(array):
+#     global sortTime
+#     startTime = time.perf_counter()
+#     for i in range(len(array)):
+#             for j in range(0, len(array) - i - 1):
+#                 if array[j] > array[j + 1]:
+#                     array[j], array[j + 1] = array[j + 1], array[j]
+#                     sortTime = time.perf_counter() - startTime
+#                     data.update(array : sortTime)
+#                     print(data)
+
+#bubbleSort(array)
