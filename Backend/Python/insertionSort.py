154314bab270332323320e5fa9bf983100250946
PyricRL <ljcosa@icloud.com>
2025-04-08 11:03:52 -0500

    replaced row with col

--- Backend/Python/insertionSort.py
+++ Backend/Python/insertionSort.py
@@ -11,5 +11,4 @@ with open(globalVariables.outputFile, mode='w', newline='') as file: # Clear fil
     pass
 
 def insertionSort(array, length):
-    for i in range(1, length - 1):
-        key = array[i]
+    pass
